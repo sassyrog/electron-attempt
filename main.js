a8b2e2af684a92366a301389fc84febc4f3382fe
Rogerndaba <rogerndaba@gmail.com>
2022-02-22 20:34:31 +0200

    electron with tailwind setup

--- main.js
+++ main.js
@@ -1,15 +1,19 @@
 // Modules to control application life and create native browser window
-const {app, BrowserWindow} = require('electron')
+const { app, BrowserWindow } = require('electron')
 const path = require('path')
 
-function createWindow () {
+function createWindow() {
   // Create the browser window.
   const mainWindow = new BrowserWindow({
     width: 800,
     height: 600,
+    minHeight: 600,
+    minWidth: 800,
     webPreferences: {
       preload: path.join(__dirname, 'preload.js')
-    }
+    },
+    // titleBarStyle: 'hiddenInset'
+    frame: false
   })
 
   // and load the index.html of the app.
@@ -41,3 +45,7 @@ app.on('window-all-closed', function () {
 
 // In this file you can include the rest of your app's specific main process
 // code. You can also put them in separate files and require them here.
+
+try {
+  require('electron-reloader')(module)
+} catch (_) { }
